27b47f99d79e81588d85e39a5848a440181b8f9d
utkudenizal@gmail.com <utkudenizal@gmail.com>
2023-01-12 00:32:23 +0300

    Codes Improved

--- ArduinoCodes_Final/2kWBonus_60akadarsoftstart/2kWBonus_60akadarsoftstart.ino
+++ ArduinoCodes_Final/2kWBonus_60akadarsoftstart/2kWBonus_60akadarsoftstart.ino
@@ -6,6 +6,6 @@ void setup() {
 void loop() {
     delay(1000);
     a = a + 3;
-    a = constrain(a,0,191);
+    a = constrain(a,1,191);
     analogWrite(5,a); // ilk sayı hangi pin olduğ, ikinci sayı 0 ile 1023 arasında Duty cycle veriyor linear ilişki 0 da %0 1023 te %100
   }
